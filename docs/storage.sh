8c3c58493aae7ffef51c5dafa0a6c5fdcc02a733
Helber Maciel Guerra <helber@cianet.ind.br>
2015-07-20 15:51:00 -0300

    Criação "não automatica" de storage

---
+++ docs/storage.sh
@@ -0,0 +1,19 @@
+
+# Particionar
+gdisk /dev/sda
+# Criar partições
+pvcreate /dev/sda1
+# Criar volume fisico
+vgcreate COREOS /dev/sda1
+# Ativar
+vgchange -ay
+# Criar volumes logicos
+lvcreate -n toolbox COREOS -L 20G
+lvcreate -n docker COREOS -L 60G
+# Formatar
+mkfs.btrfs -L TOOLBOX /dev/mapper/COREOS-toolbox
+mkfs.btrfs -L DOCKER /dev/mapper/COREOS-docker
+# Montar
+systemctl start  /var/lib/docker/
+systemctl start  /var/lib/toolbox/
+
